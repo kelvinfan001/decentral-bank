fde573991ba0cc401a95c20decd67039ef66021b
Kelvin Fan <kelvinfan001@gmail.com>
2019-02-22 17:48:50 -0500

    deposit functionality works with UI

---
+++ web/PiggyBankABI.js
@@ -0,0 +1,87 @@
+let abi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "approver",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0x141a8dd8"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "sender",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0x67e404ce"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "receiver",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0xf7260d3e"
+    },
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "deposit",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function",
+        "signature": "0xd0e30db0"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "viewApprover",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "pure",
+        "type": "function",
+        "signature": "0xd93ce89c"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_receiver",
+                "type": "address"
+            }
+        ],
+        "name": "withdraw",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function",
+        "signature": "0x51cff8d9"
+    }
+]
