03ed4a6bc41283fab45614bece187877999c8cb4
Kelvin Fan <kelvinfan001@gmail.com>
2019-02-23 01:12:35 -0500

    updated unit tests

--- contract/contracts/PiggyBank.sol
+++ contract/contracts/PiggyBank.sol
@@ -2,21 +2,23 @@ pragma solidity ^0.5.0;
 
 contract PiggyBank {
 
-    address public sender;
     address payable public receiver;
-    address public constant approver = 0x107cec7777c0CBA5f9A9AdB0E64696f93Cb45FF8;
+    address public owner;
 
-    function deposit() external payable {
-        require(msg.value > 0);
-        sender = msg.sender;
+    constructor() public {
+        owner = msg.sender;
     }
 
-    function viewApprover() external pure returns(address){
-        return(approver);
+    function deposit() external payable {
+        require(msg.value > 0, "You cannot deposit 0.");
+    }
+
+    function viewOwner() external view returns(address){
+        return(owner);
     }
 
     function withdraw(address payable _receiver) external {
-        require(msg.sender == approver);
+        require(msg.sender == owner, "Sender is not the owner of this piggy bank.");
         receiver = _receiver;
         receiver.transfer(address(this).balance);
     }
--- contract/test/PiggyBankContract.js
+++ contract/test/PiggyBankContract.js
@@ -1,25 +1,32 @@
 const PiggyBankContract = artifacts.require('../../contracts/PiggyBank.sol');
 
-contract('PiggyBankContract', function (accounts) {
+
+contract('PiggyBankContract', function () {
     it('initiates contracts', async function(){
-        const contract = await PiggyBankContract.deployed();
-        const approver = await contract.approver.call();
-        assert.equal(approver, 0x107cec7777c0CBA5f9A9AdB0E64696f93Cb45FF8, "approvers don't match");
+        const accounts = await web3.eth.getAccounts();
+        const contract = await PiggyBankContract.new({from: accounts[1]});
+        const owner = await contract.owner.call();
+        assert.equal(owner, accounts[1], "owners don't match");
     });
 
     it('takes a deposit', async function(){
-        var accounts = await web3.eth.getAccounts();
-        let contract = await PiggyBankContract.deployed();
+        const accounts = await web3.eth.getAccounts();
+        let contract = await PiggyBankContract.new({from: accounts[0]});
         await contract.deposit({ value: 1, from: accounts[0] });
         let balance = await web3.eth.getBalance(contract.address);
         assert.equal(balance.valueOf(), 1, "amounts did not match");
     });
 
     it('withdraws', async function(){
-        var accounts = await web3.eth.getAccounts();
-        let contract = await PiggyBankContract.deployed();
-        await contract.withdraw(accounts[0], { from: accounts[0]});
-        let balance = await web3.eth.getBalance(contract.address);
-        assert.equal(balance.valueOf(), 0, "amounts did not match");
+        const accounts = await web3.eth.getAccounts();
+        let contract = await PiggyBankContract.new({from: accounts[1]});
+        await contract.deposit({value: 1, from: accounts[1]});
+
+        try {
+            await contract.withdraw(accounts[0], { from: accounts[0]});
+        } catch (error) {
+            err = error;
+        }
+        assert.ok(err instanceof Error, "should not have had permission to withdraw");
     })
 });
